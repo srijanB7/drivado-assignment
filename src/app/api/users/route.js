867d07477fc842d519154b701d6e1343c0f6fd33
srijanB7 <srijanbiswas955@gmail.com>
2024-11-25 02:15:04 +0530

    removed route handlers

--- src/app/api/users/route.js
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  const res = await fetch("https://673736a9aafa2ef222330e54.mockapi.io/users");
-  const users = await res.json();
-
-  return Response.json({ users });
-}
